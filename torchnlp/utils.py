5eb9d878863db0b195c5969852ca7d137ca56a48
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-05 00:22:04 +0000

    Adding numeric_owner as keyword arguement

--- torchnlp/utils.py
+++ torchnlp/utils.py
@@ -307,7 +307,7 @@ def maybe_extract(compressed_filename, destination, extension=None):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, path=destination)
